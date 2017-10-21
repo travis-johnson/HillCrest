f920a97c19a08be316f62f002f368e7de7382604
Travis Johnson <trav.johnson919@gmail.com>
2017-10-21 18:07:41 -0400

    Dropdown menu works with sub menu options.  Need to style sub-menu

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-        var width = $(window).width();
-        if (width <= 1024) {
-            $("#menu-button").click(function() {
-                $(".menu-options").toggleClass("visible");
-            });
-        } else if (width >= 1024) {
-            $(".menu-options").show();
-        }
+        $(document).ready(function() {
+            var width = $(window).width();
+            if (width <= 1024) {
+                $(".menu-button").click(function() {
+                        $(".menu-options").toggleClass("visible");
+                    }),
+                    $('.sub-btn').click(function() {
+                        $(this).next('.sub-menu-opt').toggleClass('sub-drop');
+
+                    });
+
+
+
+            } else if (width >= 1024) {
+                $('.menu-options').show();
+            }
+        });
+
+        $('.sub-btn').click(function(e) {
+            e.stopPropagation();
+        })
+
+
+        // $('.sub-btn').click(function(){
+        //     if($('.sub-menu-opt').css('display', 'none')){
+        //     $('.sub-menu-opt').addClass('visible');
+
+        //     }
+        // });
