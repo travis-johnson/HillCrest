4bf5a77f6367cd374c822012d2ec3533fca40bd1
Travis Johnson <trav.johnson919@gmail.com>
2017-10-23 13:00:39 -0400

    refactored jquery to not use the width indicator for testing responsiveness.

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-        $(document).ready(function() {
-            var width = $(window).width();
-            if (width <= 1024) {
-                $(".menu-button").click(function() {
-                        $(".menu-options").toggleClass("visible");
-                    }),
-                    $('.sub-btn').click(function() {
-                        $(this).next('.sub-menu-opt').toggleClass('sub-drop');
+        // $(document).ready(function() {
+        //     var width = $(window).width();
+        //     if (width <= 1024) {
+        //         $(".menu-button").click(function() {
+        //                 $(".menu-options").toggleClass("visible");
+        //             }),
+        //             $('.sub-btn').click(function() {
+        //                 $(this).next('.sub-menu-opt').toggleClass('sub-drop');
 
-                    });
-
-
-
-            } else if (width >= 1024) {
-
-                $('.sub-btn').click(function() {
-                    $(this).next('.sub-menu-opt').toggleClass('sub-drop-dt');
-                });
-
-            }
-
-            $('sub-btn').click(function(e) {
-                e.preventDefault();
-            })
-        });
+        //             });
 
 
 
+        //     } else if (width >= 1024) {
 
-        // $('.sub-btn').click(function(){
-        //     if($('.sub-menu-opt').css('display', 'none')){
-        //     $('.sub-menu-opt').addClass('visible');
-
+        //       
         //     }
-        // });
+
+
+        $(".menu-button").click(function() {
+            $(".menu-options").toggleClass("visible");
+        });
+        $('.sub-btn').click(function() {
+            $(this).next('.sub-menu-opt').toggleClass('sub-drop');
+            $(this).next('.sub-menu-opt').toggleClass('sub-drop-dt');
+
+        });
