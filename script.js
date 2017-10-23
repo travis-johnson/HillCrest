67126754ba4eb021b74910ea59053d12e19b847e
Travis Johnson <trav.johnson919@gmail.com>
2017-10-23 16:18:38 -0400

    Fine tuning of styling and refactoring of jquery code.

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-        // $(document).ready(function() {
-        //     var width = $(window).width();
-        //     if (width <= 1024) {
-        //         $(".menu-button").click(function() {
-        //                 $(".menu-options").toggleClass("visible");
-        //             }),
-        //             $('.sub-btn').click(function() {
-        //                 $(this).next('.sub-menu-opt').toggleClass('sub-drop');
-
-        //             });
-
-
-
-        //     } else if (width >= 1024) {
-
-        //       
-        //     }
-
-
-        $(".menu-button").click(function() {
-            $(".menu-options").toggleClass("visible");
-        });
-        $('.sub-btn').click(function() {
-            $(this).next('.sub-menu-opt').toggleClass('sub-drop');
-            $(this).next('.sub-menu-opt').toggleClass('sub-drop-dt');
-
-        });
+$(document).ready(function() {
+    $(".menu-button").click(function() {
+        $(".menu-options").toggleClass("visible");
+    });
+    $('.sub-btn').click(function() {
+        $(this).next('.sub-menu-opt').toggleClass('sub-drop');
+        $(this).next('.sub-menu-opt').toggleClass('sub-drop-dt');
+    });
+})
